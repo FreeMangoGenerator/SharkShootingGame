4912b47d656ed5aa8ca97ab04bfc143a3acb1b40
BeemsChurger67 <toivo.daniel.harju@gmail.com>
2024-03-27 19:30:10 +0200

    Shiver me pickle

---
+++ EpicSharkShootingGameAarni/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnInterval = 2f;
+    float nextSpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time < nextSpawnTime){
+            return;
+        }
+        SpawnEnemy();
+    }
+    void SpawnEnemy(){
+        GameObject enemy = EnemyPoolManager. Instance.GetEnemy();
+        enemy.transform.position = transform.position;
+        nextSpawnTime = Time.time + spawnInterval;
+
+    }
+}
