fee771f9372a3c48b5ab6314c6ecbce7e735bf62
BeemsChurger67 <toivo.daniel.harju@gmail.com>
2024-04-17 19:29:09 +0300

    commit

--- EpicSharkShootingGameAarni/Assets/Scripts/Enemy/Enemy.cs
+++ EpicSharkShootingGameAarni/Assets/Scripts/Enemy/Enemy.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamageable
 {
     float currentSpeed = 3f;
+    public int MaxHP = 3;
+
+    private int currentHP = 0;
+    public float attackRange = 5f;
+    public int attackPower = 1;
+    public float dashSpeed = 10f;
+    public float dashDuration = 0.5f;
+    private bool isDashing = false;
+    public float attackTimer = 0f;
     Rigidbody2D body;
     public Transform playerTransform;
     // (:-|)
@@ -12,7 +21,9 @@ public class Enemy : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
     }
-
+    void onEnable() {
+        currentHP = MaxHP;
+    }
     // ("_"<(ZZZZZ))
     void FixedUpdate()
     {
@@ -31,5 +42,22 @@ public class Enemy : MonoBehaviour
     }
     void GetPlayer(){
         playerTransform = GameManager.Instance.GetPlayer.transform;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        if(currentHP <= 0){
+            Die();
+        }
+    }
+    public void Die()
+    {
+        EnemyPoolManager.Instance.ReturnEnemy(gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other){
+        IDamageable damageable = other.GetComponent<IDamageable>();
+        if(damageable != null){
+            damageable.TakeDamage(1);
+        }
     }
 }
