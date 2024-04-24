3797b234086adaae6114c27199d018770e098010
BeemsChurger67 <toivo.daniel.harju@gmail.com>
2024-04-24 19:29:48 +0300

    blubber

--- EpicSharkShootingGameAarni/Assets/Scripts/Enemy/Enemy.cs
+++ EpicSharkShootingGameAarni/Assets/Scripts/Enemy/Enemy.cs
@@ -12,10 +12,13 @@ public class Enemy : MonoBehaviour, IDamageable
     public int attackPower = 1;
     public float dashSpeed = 10f;
     public float dashDuration = 0.5f;
+    public float attackCooldown = 2f;
     private bool isDashing = false;
     public float attackTimer = 0f;
     Rigidbody2D body;
     public Transform playerTransform;
+
+    Vector2 direction;
     // (:-|)
     void Awake()
     {
@@ -23,20 +26,58 @@ public class Enemy : MonoBehaviour, IDamageable
     }
     void onEnable() {
         currentHP = MaxHP;
+        isDashing = false;
+    }
+    void OnDisable() {
+        StopAllCoroutines();
     }
     // ("_"<(ZZZZZ))
     void FixedUpdate()
     {
         
          Move();
+         Attack();
+           }
+
+private void Attack(){
+    if(playerTransform == null)
+    {
+        return;
+    }
+    if(attackTimer > 0){
+        attackTimer -= Time.fixedDeltaTime;
+    }
+    else if(!isDashing && Vector2.Distance(transform.position, playerTransform.position) < attackRange)
+    {
+        StartCoroutine(DashAttack());
     }
 
+    IEnumerator DashAttack(){
+
+        isDashing = true;
+        float startTime = Time.time;
+        while(Time.time < startTime + dashDuration){
+            body.velocity = direction * dashSpeed;
+            yield return null;
+        }
+        
+
+        
+        body.velocity = Vector2.zero;
+        isDashing = false;
+        attackTimer= attackCooldown;
+    }
+}
     void Move(){
         if(playerTransform == null){
             GetPlayer();
             return;
+            
         }
-        Vector2 direction = (playerTransform.position - transform.position).normalized;
+        if(isDashing){
+            return;
+        }
+        direction = (playerTransform.position - transform.position).normalized;
         body.MovePosition(body.position + direction * currentSpeed * Time.fixedDeltaTime);
     
     }
